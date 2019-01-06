6fc8636931b47855636b5ac270e7fb63d631b160
Aldiantoro Nugroho <kriwil@gmail.com>
2019-01-06 19:05:53 +0700

    Bump version: 0.3.4 → 0.3.5

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name="littleauth",
-    version="0.3.4",
+    version="0.3.5",
     description="Custom User model with uuid as primary key, and email as username.",
     long_description=readme + "\n\n" + history,
     author="Aldiantoro Nugroho",
